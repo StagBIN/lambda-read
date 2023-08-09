851e955f6a068645b09bc5c87d2bf369f600b04d
Vjs Pranavasri <pranavasri@live.in>
2023-08-09 11:48:52 +0530

    Add support for encryption
    * Send back whether data is encrypted or not

--- index.js
+++ index.js
@@ -8,12 +8,18 @@ exports.handler = async(event) => {
     const params = {
         TableName: tableName,
         KeyConditionExpression: 'id = :hashKey',
+        //ProjectionExpression: "#id, #data, #url",
         ExpressionAttributeValues: {
             ':hashKey': event.pathParameters.id,
-        }
+        },
+        // ExpressionAttributeNames: {
+        //     "#data": "data",
+        //     "#id": "id",
+        //     "#url": "url"
+        // },
     };
 
-    const data = await documentClient.query(params).promise();
+    var data = await documentClient.query(params).promise();
 
     if (!data.Items.length) {
         return {
@@ -24,11 +30,24 @@ exports.handler = async(event) => {
             body: JSON.stringify({ error: "Not found" })
         };
     }
-
+    
+    if (event.headers.buid == data.Items[0].buid) {
+        data.Items[0].edit = true;
+    } else {
+        data.Items[0].edit = false;
+    }
+    
+    delete data.Items[0].buid;
+    
+    // if(!data.Items[0].isEncrypted){
+        data.Items[0].isEncrypted = data.Items[0].isEncrypted || false;
+    // }
+    
     const response = {
         statusCode: 200,
         headers: {
-            'Access-Control-Allow-Origin': '*'
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, buid'
         },
         body: JSON.stringify(data.Items),
     };
